c0bc1478dd67d038720aa9da6f15ef20a5c03b3d
Iceprincess2 <favour.adjushi@decagon.dev>
2024-01-09 23:42:14 +0100

    styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { StatusBar } from 'expo-status-bar'
-import {Bars3CenterLeftIcon} from "react-native-heroicons/outline"
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Bars3CenterLeftIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <StatusBar style="light"/>
-        <View>
-          <Bars3CenterLeftIcon />
-
+        <StatusBar style="light" />
+        <View style={styles.bar}>
+          <Bars3CenterLeftIcon style={styles.barIcon} />
+          <Text></Text>
         </View>
       </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'rgb(51, 51, 51)',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(51, 51, 51)",
+  },
+  bar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: "4",
+  },
+  barIcon: {
+    fontSize: "30px",
+    strokeWidth: "2px",
+    color: "white",
+
+  },
+});
